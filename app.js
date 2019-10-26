c3eb4aac2d99317b86621f4146b1bdfc5a266c41
Bart Frackiewicz <decomplosed@gmail.com>
2019-10-26 20:57:37 +0200

    ES 6 Classes

--- app.js
+++ app.js
@@ -228,3 +228,42 @@
 // console.log(bart)
 
 // console.log(bart.greeting())
+
+
+
+
+// // 6) ES6 Classes
+
+// class Person {
+//   constructor(firstName, lastName, dob) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.birthday = new Date(dob)
+//   }
+
+//   greeting() {
+//     return `Hello there ${this.firstName} ${this.lastName}`
+//   }
+
+//   calculateAge() {
+//     const diff = Date.now() - this.birthday.getTime()
+//     const ageDate = new Date(diff)
+//     return Math.abs(ageDate.getUTCFullYear() - 1970)
+//   }
+
+//   getsMarried(newLastName) {
+//     this.lastName = newLastName
+//   }
+
+//   static addNumbers(x, y) {
+//     return x + y
+//   }
+// }
+
+// const mary = new Person('Mary', 'Williams', '9-29-1993')
+
+// mary.getsMarried('Manning')
+
+// console.log(mary);
+
+// console.log(Person.addNumbers(1, 2))
